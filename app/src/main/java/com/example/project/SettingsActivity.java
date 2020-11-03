89280fd227e2a64b12b73385e1ed0d197c4855da
Linh Tuan <linhbun2@gmail.com>
2020-11-03 13:39:56 +0700

    update thumb_image field in Firebase

--- app/src/main/java/com/example/project/SettingsActivity.java
+++ app/src/main/java/com/example/project/SettingsActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.StringBuilderPrinter;
@@ -27,13 +28,20 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.iceteck.silicompressorr.SiliCompressor;
 import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -81,14 +89,15 @@ public class SettingsActivity extends AppCompatActivity {
                 mName.setText(name);
                 mStatus.setText(status);
 
-                // Use library Picasso to get image from internet
-                Picasso.get().load(image).into(mDisplayImage);
+                if (image.equals("default")) {
+                    // Use library Picasso to get image from internet
+                    Picasso.get().load(image).placeholder(R.drawable.default_avatar).into(mDisplayImage);
+                } else {
+                    Picasso.get().load(image).into(mDisplayImage);
+                }
             }
-
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
+            public void onCancelled(@NonNull DatabaseError error) { }
         });
 
         mStatusBtn.setOnClickListener(new View.OnClickListener() {
@@ -108,18 +117,12 @@ public class SettingsActivity extends AppCompatActivity {
                 galleryIntent.setType("image/*");
                 galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(galleryIntent, "Select Image"), GALLERY_PICK);
-
-                /*
-                CropImage.activity()
-                        .setGuidelines(CropImageView.Guidelines.ON)
-                        .start(SettingsActivity.this);
-                */
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
@@ -140,8 +143,25 @@ public class SettingsActivity extends AppCompatActivity {
                 mProgress.show();
 
                 Uri resultUri = result.getUri();
+                File thump_filePath = new File(resultUri.getPath());
                 String current_user_id = mCurrentUser.getUid();
+
+                Bitmap thumb_bitmap = null;
+                try {
+                    thumb_bitmap = new Compressor(this)
+                                .setMaxWidth(200)
+                                .setMaxHeight(200)
+                                .compressToBitmap(thump_filePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                thumb_bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                final byte[] thumb_byte = baos.toByteArray();
+
                 final StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id + ".jpg");
+                final StorageReference thumb_filepath = mImageStorage.child("profile_images").child("thumb").child(current_user_id + ".jpg");
+
                 filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
@@ -149,14 +169,32 @@ public class SettingsActivity extends AppCompatActivity {
                             filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                                 @Override
                                 public void onSuccess(Uri uri) {
-                                    String download_url = uri.toString();
-                                    mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    final String download_url = uri.toString();
+                                    final String thumb_downloadUrl = uri.toString();
+                                    UploadTask uploadTask = thumb_filepath.putBytes(thumb_byte);
+                                    uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                                         @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> thumb_task) {
+                                            if (thumb_task.isSuccessful()) {
+
+                                                Map update_haspMap = new HashMap();
+                                                update_haspMap.put("image", download_url);
+                                                update_haspMap.put("thumb_image", thumb_downloadUrl);
+
+                                                mUserDatabase.updateChildren(update_haspMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            mProgress.dismiss();
+                                                            Toast.makeText(SettingsActivity.this,
+                                                                    "Upload complete. Change your image successfully.",
+                                                                    Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
+                                            } else {
                                                 mProgress.dismiss();
-                                                Toast.makeText(SettingsActivity.this,
-                                                        "Upload complete. Change your image successfully.",
+                                                Toast.makeText(SettingsActivity.this, "Errors in uploading to thumbnail.",
                                                         Toast.LENGTH_SHORT).show();
                                             }
                                         }
@@ -173,17 +211,5 @@ public class SettingsActivity extends AppCompatActivity {
                 Exception error = result.getError();
             }
         }
-    }
-
-    public static String random() {
-        Random generator = new Random();
-        StringBuilder randomStringBuilder = new StringBuilder();
-        int randomLength = generator.nextInt(10);
-        char tempChar;
-        for (int i = 0; i < randomLength; i ++) {
-            tempChar = (char) (generator.nextInt(96) + 32);
-            randomStringBuilder.append(tempChar);
-        }
-        return randomStringBuilder.toString();
     }
 }
---
+++ app/src/main/java/com/example/project/UsersActivity.java
@@ -0,0 +1,102 @@
+package com.example.project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class UsersActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView mUsersList;
+
+    public DatabaseReference mUsersDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+        mToolbar = findViewById(R.id.users_appBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("All Users");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("name");
+
+        mUsersList = findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Users> options = new FirebaseRecyclerOptions.Builder<Users>().setQuery(mUsersDatabase, Users.class).build();
+
+        FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users users) {
+                holder.setName(users.getName());
+                holder.setStatus(users.getStatus());
+            }
+
+            @NonNull
+            @Override
+            public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view;
+                RecyclerView.ViewHolder viewHolder = null;
+                if (viewType == R.id.users_list) {
+                    view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_single_layout, parent, false);
+                    viewHolder = new UsersViewHolder.ViewHolderOne(view);
+                }
+                return (UsersViewHolder) viewHolder;
+            }
+        };
+
+        mUsersList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setName(String name){
+            TextView userNameView = mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+        }
+
+        public void setStatus(String status) {
+            TextView userStatusView = mView.findViewById(R.id.user_single_status);
+            userStatusView.setText(status);
+        }
+
+        public static class ViewHolderOne extends RecyclerView.ViewHolder {
+            View view;
+            public ViewHolderOne(View itemView) {
+                super(itemView);
+                view = itemView.findViewById(R.id.users_list);
+            }
+        }
+
+    }
+}
